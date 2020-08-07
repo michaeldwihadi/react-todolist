aea0c3edc292693f0de5d56c0475ab8c3313450a
Michael <51073271+michaeldwihadi@users.noreply.github.com>
2020-08-07 18:17:54 +0800

    CSS Component Module

---
+++ src/components/todoform/TodoForm.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./todoform.module.css";
+
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("Please Fill In !");
+      return;
+    }
+
+    if (value.length > 40) {
+      alert("Please create a shorter todo text !");
+      setValue("");
+      return;
+    }
+
+    addTodo(value);
+    setValue("");
+  };
+
+  if (showAdd) {
+    return (
+      <section className={styles.add}>
+        <form className={styles.addForm} onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className={styles.addInput}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button className={styles.addBtn}>Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
+};
+
+export default TodoForm;
